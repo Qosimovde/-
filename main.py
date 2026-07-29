93f81394c63339dc15f4dbd946dc0fae2c40c9bc
Qosimovde <Qosimovde@gmail.com>
2026-07-29 14:57:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 import yt_dlp
 
-TOKEN = "8549085903:AAGe2LmXahcCXxZzdOsF0oAB-h0g3j8Ilvg"
+TOKEN ="8549085903:AAGe2LmXahcCXxZzdOsF0oAB-h0g3j8Ilvg"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
